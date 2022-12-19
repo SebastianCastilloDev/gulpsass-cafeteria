5e2cb702aa6305d00b847a972b92b4dff525c4a1
Sebastian Castillo <tantrum5535@gmail.com>
2022-12-19 18:53:32 -0300

    corriendo multiples tareas en gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
-const {src, dest, watch } = require('gulp')
+const {src, dest, watch, series, parallel } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+
 
 function css(done) {
 
     src('src/scss/app.scss')
         .pipe( sass({outputStyle: 'compressed'}))
+        .pipe( postcss([autoprefixer()]))
         .pipe( dest('build/css'))
-
     done()
 }
 
@@ -14,5 +17,13 @@ function dev(){
     watch( 'src/scss/app.scss', css)
 }
 
-exports.dev = dev
+function tareaDefault(cb) {
+    console.log('hola');
+    cb()
+}
+
 exports.css = css
+exports.dev = dev
+
+exports.default = series(css, dev)
+
