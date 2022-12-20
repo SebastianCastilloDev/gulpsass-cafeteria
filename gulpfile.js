84e2f498b368d93ca1cab86e1260529a3834577d
Sebastian Castillo <tantrum5535@gmail.com>
2022-12-19 22:57:53 -0300

    aligerar imagenes con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const {src, dest, watch, series, parallel } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
+const imagemin = require('gulp-imagemin')
 
 
 function css(done) {
@@ -13,13 +14,22 @@ function css(done) {
     done()
 }
 
+function imagenes(done) {
+    src('src/img/**/*')
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest('build/img'))
+    done()
+}
+
+
 function dev(){
     watch('src/scss/**/*.scss', css)
+    watch('src/img/**/*', imagenes)
 }
 
 
 exports.css = css
 exports.dev = dev
-
-exports.default = series(css, dev)
+exports.imagenes = imagenes
+exports.default = series(imagenes, css, dev)
 
